152af5de5981c40d5eae72da1847798c54041f4b
k-kbk <kkbk-@naver.com>
2022-12-15 11:46:08 +0900

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      spacing: {
+        88: '22rem',
+        104: '26rem',
+        112: '28rem',
+        128: '32rem',
+      },
+    },
+  },
+  plugins: [],
+};
