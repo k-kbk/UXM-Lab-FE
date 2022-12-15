133c237323352fde229b910e3f3183828777a645
k-kbk <kkbk-@naver.com>
2022-12-15 21:55:54 +0900

    feat: 라우트 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layouts/Layout';
 import Home from './pages/Home';
+import Members from './pages/Members';
+import Studies from './pages/Studies';
+import Publications from './pages/Publications';
+import News from './pages/News';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/members' element={<Members />} />
+        <Route path='/studies' element={<Studies />} />
+        <Route path='/publications' element={<Publications />} />
+        <Route path='/news' element={<News />} />
       </Routes>
     </Layout>
   );
