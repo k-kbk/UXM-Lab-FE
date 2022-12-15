eaf547ca3bc330fcb50894b2d6aca33e61c058c1
k-kbk <kkbk-@naver.com>
2022-12-15 21:55:34 +0900

    design: 번호 추가

--- src/components/home/ContactUs.jsx
+++ src/components/home/ContactUs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Call from '../../assets/call.png';
 import EMail from '../../assets/email.png';
 import Address from '../../assets/address.png';
 
@@ -10,6 +11,12 @@ const ContactUs = () => {
           <h2 className='heading'>Contact Us</h2>
         </div>
         <div className='card-container'>
+          <div className='card'>
+            <img src={Call} alt='call' className='w-10 h-10 mb-3' />
+            <h3 className='font-semibold text-2xl mb-5'>Call</h3>
+            <p className='font-medium text-lg'>02-300-0000</p>
+            <p className='font-medium text-lg'>02-303-3333</p>
+          </div>
           <div className='card'>
             <img src={EMail} alt='mail' className='w-10 h-10 mb-3' />
             <h3 className='font-semibold text-2xl mb-5'>E-Mail</h3>
