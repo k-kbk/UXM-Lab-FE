2b663c4b88dd125afbe5160bcad2f69afda0596a
k-kbk <kkbk-@naver.com>
2022-12-15 14:07:03 +0900

    chore: tailwindcss 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
     extend: {
+      colors: {
+        myIndigo: '#0D4C92',
+      },
       spacing: {
+        18: '4.5rem',
+        30: '7.5rem',
         88: '22rem',
         104: '26rem',
         112: '28rem',
