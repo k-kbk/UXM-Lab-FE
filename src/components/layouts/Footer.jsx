7ce461654412963a73270ac6bd981fafd90d99c3
k-kbk <kkbk-@naver.com>
2022-12-15 20:45:16 +0900

    feat: Footer 컴포넌트 구현

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='footer'>
-      <div className='footer-container'></div>
+      <div className='footer-container'>
+        <p className='text-sm'>© 2022 UXM Lab & bk. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
