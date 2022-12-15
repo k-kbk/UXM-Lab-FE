93a0d07ad423da29704975379079cebb4b4946eb
k-kbk <kkbk-@naver.com>
2022-12-15 20:44:23 +0900

    feat: Home 컴포넌트 article-3 구현

--- src/components/home/ContactUs.jsx
+++ src/components/home/ContactUs.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import EMail from '../../assets/email.png';
+import Address from '../../assets/address.png';
 
 const ContactUs = () => {
   return (
-    <article className='home-article__3'>
-      <div className='content__3'></div>
+    <article className='home-article_3'>
+      <div className='article-container_3'>
+        <div className='mb-30 text-center'>
+          <h2 className='heading'>Contact Us</h2>
+        </div>
+        <div className='card-container'>
+          <div className='card'>
+            <img src={EMail} alt='mail' className='w-10 h-10 mb-3' />
+            <h3 className='font-semibold text-2xl mb-5'>E-Mail</h3>
+            <p className='font-medium text-lg'>Prof: example@gmail.com</p>
+            <p className='font-medium text-lg'>Lab: example@gmail.com</p>
+          </div>
+          <div className='card'>
+            <img src={Address} alt='address' className='w-10 h-10 mb-3' />
+            <h3 className='font-semibold text-2xl mb-5'>Address</h3>
+            <p className='font-medium text-lg'>명지대학교 인문캠퍼스</p>
+            <p className='font-medium text-lg'>학생회관 10층, 10103호</p>
+          </div>
+        </div>
+      </div>
     </article>
   );
 };
