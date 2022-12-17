83cc2ff752c459646633526bca83a55c3269fd41
k-kbk <kkbk-@naver.com>
2022-12-17 15:49:01 +0900

    chore: tailwindcss 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
         104: '26rem',
         112: '28rem',
         128: '32rem',
+        mobileScreen: 'calc(var(--vh, 1vh) * 100)',
       },
       keyframes: {
         bounceSlow: {
