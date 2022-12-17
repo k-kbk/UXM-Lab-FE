72b81b69b8ede10261f3f02421590a82f617cb89
k-kbk <kkbk-@naver.com>
2022-12-17 20:07:15 +0900

    feat: Link 추가

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Nav from './Nav';
+import Menu from '../../assets/menu.png';
 
 const Header = () => {
   return (
     <header className='header'>
       <div className='header-container'>
         <div>
-          <p className='font-bold text-2xl sm:text-3xl '>uxm lab.</p>
+          <Link to='/' className='font-bold text-2xl sm:text-3xl'>
+            uxm lab.
+          </Link>
+        </div>
+        <div className='block md:hidden'>
+          <img src={Menu} alt='menu' className='w-6 h-6' />
         </div>
         <Nav />
       </div>
