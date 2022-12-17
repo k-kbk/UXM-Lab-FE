d9e4cb306fa0f9be96b7bfe455f6d6a41b463a5b
k-kbk <kkbk-@naver.com>
2022-12-17 15:49:29 +0900

    feat: 모바일 100vh 호환 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layouts/Layout';
 import Home from './pages/Home';
-import Members from './pages/Members';
-import Studies from './pages/Studies';
 import Publications from './pages/Publications';
-import News from './pages/News';
+
+var vh = 0;
 
 function App() {
+  useEffect(() => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
+
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/members' element={<Members />} />
-        <Route path='/studies' element={<Studies />} />
+        {/* <Route path='/members' element={<Members />} /> */}
+        {/* <Route path='/studies' element={<Studies />} /> */}
         <Route path='/publications' element={<Publications />} />
-        <Route path='/news' element={<News />} />
+        {/* <Route path='/news' element={<News />} /> */}
       </Routes>
     </Layout>
   );
