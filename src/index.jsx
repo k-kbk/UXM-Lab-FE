8243caa2a0c0ef6a5d24c2e86ff225efd4d15de3
k-kbk <kkbk-@naver.com>
2022-12-21 13:44:29 +0900

    chore: React Query 버전 변경

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import './styles/global.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
