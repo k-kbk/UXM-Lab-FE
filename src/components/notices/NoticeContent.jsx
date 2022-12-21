c23a040d91e961020121507fc1096e835072bf1f
k-kbk <kkbk-@naver.com>
2022-12-21 15:30:58 +0900

    refactor: key 속성 추가

--- src/components/notices/NoticeContent.jsx
+++ src/components/notices/NoticeContent.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const NoticeContent = ({ dummy }) => {
   return (
     <div className='notice-detail_content'>
-      {dummy.map((line) => (
-        <>
-          <p className='notice-detail_line'>{line}</p>
-        </>
+      {dummy.map((line, idx) => (
+        <p key={idx} className='notice-detail_line'>
+          {line}
+        </p>
       ))}
     </div>
   );
